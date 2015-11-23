8d890742be3c0f5234d4aa1a07e669f7ab1e095b
TOBY <tmletcher@gmail.com>
2015-11-23 16:52:16 +1100

    random

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,9 +1,5 @@
 class SessionController < ApplicationController
 
-  def new
-
-  end
-
   def create
 
     user = User.find_by(email: params[:email])
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Item.delete_all
+
+# Item.create(title: 'tractor', image_id: 'https://www.deere.com/common/media/images/product/tractors/r4d042782_8Rtractor_762x458.jpg', loan_status: 'onloan', price: 40.00, report: 'n/a', user_id: 1, location: '3000')
+
+# Item.create(title: 'tractor', image_id: 'http://www.teara.govt.nz/files/p18401nsil.jpg', loan_status: 'onloan', price: 40.00, report: 'n/a', user_id: 1, location: '3000')
+
+# Item.create(title: 'tractor', image_id: 'https://upload.wikimedia.org/wikipedia/commons/e/e7/John_Deere_cotton_harvester_kv02.jpg', loan_status: 'onloan', price: 40.00, report: 'n/a', user_id: 1, location: '3000')
+
+# Item.create(title: 'tractor', image_id: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQwOe6xk_ByOvOfBvUcBKTgQ66gOgNHLmkK4LpMMjpkUGaJKYmDdQ', loan_status: 'onloan', price: 40.00, report: 'n/a', user_id: 1, location: '3000')
+
+# Item.create(title: 'tractor', image_id: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS1D33kpYnKEvESyY479Ti3uL91X-92ux2NpUenMVg1GuGMfwqDJA', loan_status: 'onloan', price: 40.00, report: 'n/a', user_id: 1, location: '3000')
+
+# Item.create(title: 'tractor', image_id: 'http://i.ebayimg.com/00/s/NDgwWDY0MA==/z/BF4AAOSwZVhWRTXz/$_75.JPG', loan_status: 'onloan', price: 40.00, report: 'n/a', user_id: 1, location: '3000')
+
+# Item.new(title: 'tractor', image_id: '
+# https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQrCxWpKR7HFv5zaxKcBx_ZiSsFfK7UCR3okauxKRgKMb4NakxP', loan_status: 'onloan', price: '40.00', report: 'n/a', user_id: 1, location: '3000')
+n= Item.new
+n.title = 'tractor'
+n.save
+
+
+
+
