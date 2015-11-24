7cc007ce09e1d8dfba31abfb3521bc0cb56c5e33
Kylie Sy <kylksy@gmail.com>
2015-11-25 00:54:57 +1100

    updated item CRUD

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,13 +11,24 @@ class ItemsController < ApplicationController
     redirect_to '/items'
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to '/items'
+  end
+
   def destroy
     @item = Item.find(params[:id])
     @item.destroy
+    redirect_to '/items'
   end
 
   def item_params
-    params.require(:item).permit(params[:item].keys)
+    params.require(:item).permit(:title).merge(user_id: current_user.id)
   end
 
   def search
