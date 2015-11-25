d946947ec086f639c3fdbf4d9e318961f74d2113
TOBY <tmletcher@gmail.com>
2015-11-25 17:11:28 +1100

    even more search things

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,10 +10,10 @@ class PagesController < ApplicationController
 
   def search
     # @item = params[:search_input]
-    if !params[:search_input].empty?
+    if !params[:search_input].empty? || !params[:search_input].nil? 
       @query = params[:search_input]
       @result = Item.where("title LIKE ?", "%#{@query}%")
-      # Item.where("location LIKE ?", "%#{@query}%")
+      # Item.where("location qLIKE ?", "%#{@query}%")
       # @cover_image = ItemImage.find_by(item_id: )
     else 
       redirect_to '/'
