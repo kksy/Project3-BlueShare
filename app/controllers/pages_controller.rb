2c46d32b879b538381eb3dadcc0f86c93870316c
rc <ravithejreddy@gmail.com>
2015-11-25 16:59:03 +1100

    resolving conflicts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,16 +10,12 @@ class PagesController < ApplicationController
 
   def search
     # @item = params[:search_input]
-    if !params[:search_input].empty?
+    if params[:search_input].nil? || params[:search_input].empty?
       @query = params[:search_input]
       @result = Item.where("title LIKE ?", "%#{@query}%")
-    else
-
       # @cover_image = ItemImage.find_by(item_id: )
     else
-
       redirect_to '/'
-
     end
 
   end
