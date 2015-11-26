ea1de6b2b6c48f1ffacf9c2fa611e3568c0dde50
Kylie Sy <kylksy@gmail.com>
2015-11-26 17:18:20 +1100

    fixed reserve item

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,6 @@ class Item < ActiveRecord::Base
   has_many :item_images
 
   def location_details
-    JSON(HTTParty.get("http://v0.postcodeapi.com.au/suburbs/#{self.location }.json").body)
+    # JSON(HTTParty.get("http://v0.postcodeapi.com.au/suburbs/#{self.location }.json").body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
   resources :items
 
   namespace :api do
-    resources :items, only: [:index, :create, :destroy]
+    resources :items, only: [:index, :create, :destroy, :update]
     resources :item_images, only: [:index, :create, :destroy]
      # resources :ratings only [:index, :create, :destroy]
   end
